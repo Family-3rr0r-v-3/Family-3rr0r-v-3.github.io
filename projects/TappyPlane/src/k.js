8a398f129ec2d40df3f9b79eb3cacc1d16028923
Mishicoder <daedgamesperu@gmail.com>
2024-02-14 12:09:54 -0500

    update

--- projects/TappyPlane/src/k.js
+++ projects/TappyPlane/src/k.js
@@ -1,6 +1,33 @@
 //* Importamos la libreria desde la url
 import kaboom from 'https://unpkg.com/kaboom@3000.1.17/dist/kaboom.mjs';
 
+const w = screen.width;
+const h = screen.height;
+
+let scale = 1.0;
+
+console.log(w, h);
+
+switch(screen.orientation.type){
+    case 'landscape-primary':
+        if(h <= 400) {
+            console.log('landscape primary');
+            scale = 0.7;
+        }
+        break;
+    case 'landscape-secondary':
+        console.log('landscape secondary');
+        break;
+    case 'portrait-primary':
+        if(w <= 400) {
+            console.log('reducir escala en vertical');
+            scale = 0.5;
+        }
+        break;
+    default:
+        break;
+}
+
 //* Creamos el objeto Kaboom y le asignamos las propiedades
 //? Utilizaremos Kaboom de forma global, por lo tanto, no es necesario colocar la propiedad 'global'
 const k = kaboom({
@@ -8,7 +35,8 @@ const k = kaboom({
     height: 480, //? Alto del canvas
     canvas: document.querySelector('#game'), //* Referencia al canvas del documento HTML
     background: [218, 239, 247], //? Color de fondo de la ventana
-    debug: true
+    debug: true,
+    scale: scale
 });
 debug.inspect = false;
 audioCtx = new AudioContext();
